be4482cfba26b345127544330ae5b624df80b841
“edasenembiyik” <edasenembiyik@gmail.com>
2022-06-12 21:41:02 +0300

    no message

---
+++ myproject/src/com/mypackage/VariableNamingConvention.java
@@ -0,0 +1,40 @@
+package com.mypackage;
+
+public class VariableNamingConvention {
+	// There are many ways to make naming
+	// Some main variable naming types are given below with examples.
+
+	// 1.loverCamelCase
+
+	int mainValue = 25;
+	String firstCode = "Hello World!";
+	boolean isAvailable = true;
+
+	// 2.UpperCamelCase
+	// The Upper Camel Case naming convention is that the first letters of all words
+	// are capitalized.
+
+	String TemporaryResult = "empty";
+	float MoneyAmount = 25.564f;
+
+	// 3.Snake_Case
+	// Words are linked by underscores (_).
+
+	// 3.1 Upper_Snake_Case
+	// The first letter of words is uppercase and they are connected by _
+
+	byte Door_Number = 7;
+	short Third_Variable = 566;
+
+	// 3.2 lower_snake_case
+	// All words are lower case and they are connected by _
+
+	Long flight_number = 4453225L;
+	String my_firt_comment = "Good Job!";
+
+	// 3.3 SCREAMING_SNAKE_CASE
+	// Any variable decorated with a static final keyword combination should be
+	// written in screaming snake case
+	final String BANK_ACCOUNT = "4562_5621_5522_54526";
+	final double PI_NUMBER = 3.14;
+}
